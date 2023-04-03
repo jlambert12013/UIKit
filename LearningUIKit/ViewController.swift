66382480bf647ff61893e7b0555508f36afec185
jlambert12013 <jlambert12013@gmail.com>
2023-04-03 07:26:22 -0500

    Starting Project

---
+++ LearningUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LearningUIKit
+//
+//  Created by Jim Lambert on 4/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
