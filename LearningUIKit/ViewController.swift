31cf93f4cdce513d0da85bd6026b46afda427a7f
jlambert12013 <jlambert12013@gmail.com>
2023-04-03 08:16:39 -0500

    Create a Button using UIButton

--- LearningUIKit/ViewController.swift
+++ LearningUIKit/ViewController.swift
@@ -8,13 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    //  Making a button with a closure using UIButton
+    let myButton: UIButton = {
+        
+        // Using Button.configuration is the quickest way in UIKit
+        var config = UIButton.Configuration.filled()
+        config.title = "Click Me"
+        config.baseBackgroundColor = .systemRed
+        config.baseForegroundColor = .white
+        config.cornerStyle = .capsule
+        
+        let button = UIButton(configuration: config)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         self.view.backgroundColor = .systemMint
+        
+        self.view.addSubview(myButton)
+        
+        // Adding button to the View
+        NSLayoutConstraint.activate([
+            myButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            myButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+        
+        
     }
-
-
+    
+    
 }
 
